890e6e927a83662e69d521dbf402c3b8ca04f7b6
Mário Hatalčík <xhatalc@fi.muni.cz>
2023-10-23 11:43:15 +0200

    shared memory without copy first

--- kernel.cu
+++ kernel.cu
@@ -8,57 +8,48 @@ __global__ void copy_data(float* din, float* dout){
     dout[i] = din[i] * 6.0f;
 }
 
-__global__ void compute_lines(const float *din, float* dout, int x, int y){
+__global__ void compute_lines(const float *din, float* dout, int x, int y, float* sums){
     unsigned int line = blockIdx . x * blockDim.x + threadIdx.x;
     float z = sqrtf(3.f) - 2.f;
     float z1;
     float* myLine = dout + (line * x);
+    const float* myLineIn = din + (line * x);
 
     // compute 'sum'
-    float sum = (myLine[0] + powf(z, x)
-                             * myLine[x - 1]) * (1.f + z) / z;
-    z1 = z;
-    float z2 = powf(z, 2 * x - 2);
-    float iz = 1.f / z;
-    for (int j = 1; j < (x - 1); ++j) {
-        sum += (z2 + z1) * myLine[j];
-        z1 *= z;
-        z2 *= iz;
-    }
+    float sum = (myLineIn[0] + powf(z, x)
+                             * myLineIn[x - 1]) * 6 * (1.f + z) / z;
+    sum += sums[line] * 6;
+//    if (line == 0){
+//        printf("%f", sum);
+//    }
 
     // iterate back and forth
     myLine[0] = sum * z / (1.f - powf(z, 2 * x));
     for (int j = 1; j < x; ++j) {
-        myLine[j] += z * myLine[j - 1];
+        myLine[j] = myLineIn[j]* 6 + z * myLine[j - 1] ;
     }
-    myLine[x - 1] *= z / (z - 1.f);
+    myLine[x - 1] =  myLine[x - 1] * z / (z - 1.f);
     for (int j = x - 2; 0 <= j; --j) {
         myLine[j] = z * (myLine[j + 1] - myLine[j]);
     }
 }
 
-__global__ void compute_cols(const float *din, float* dout, int x, int y){
+__global__ void compute_cols(const float *din, float* dout, int x, int y, float * sums){
     unsigned int col = blockIdx . x * blockDim.x + threadIdx.x;
     const float z = sqrtf(3.f) - 2.f;
-    float z1;
+//    float z1;
     float *myCol = dout + col;
+    const float *myColIn = din + col;
 
     // compute 'sum'
-    float sum = (myCol[0*x] + powf(z, y)
-                              * myCol[(y - 1)*x]) * (1.f + z) / z;
-    z1 = z;
-    float z2 = powf(z, 2 * y - 2);
-    float iz = 1.f / z;
-    for (int j = 1; j < (y - 1); ++j) {
-        sum += (z2 + z1) * myCol[j*x];
-        z1 *= z;
-        z2 *= iz;
-    }
+    float sum = (myColIn[0*x] + powf(z, y)
+                              * myColIn[(y - 1)*x]) * 6 * (1.f + z) / z;
 
+    sum += sums[col] * 6;
     // iterate back and forth
     myCol[0*x] = sum * z / (1.f - powf(z, 2 * y));
     for (int j = 1; j < y; ++j) {
-        myCol[j*x] += z * myCol[(j - 1)*x];
+        myCol[j*x] = myColIn[j*x] * 6 + z * myCol[(j - 1)*x];
     }
     myCol[(y - 1)*x] *= z / (z - 1.f);
     for (int j = y - 2; 0 <= j; --j) {
@@ -66,79 +57,78 @@ __global__ void compute_cols(const float *din, float* dout, int x, int y){
     }
 }
 
+__global__ void com(const float* presums, float* sums, const float * in, int x){
+    __shared__ float cache[128];
+    float sum = 0;
+    int tid = threadIdx.x;
+    int a = x / blockDim.x;
+    int block_pos_x = blockIdx.x % a;
+    int block_pos_y = blockIdx.x / a;
+    cache[tid] = presums[block_pos_x * blockDim.x + tid];
+
+    __syncthreads();
+    for (int i = 0; i < 128; ++i){
+        int cur = x * (block_pos_y * blockDim.x + tid) + block_pos_x * blockDim.x + i;
+        sum += cache[i] * in[cur];
+    }
+
+    atomicAdd(&sums[tid + block_pos_y * blockDim.x], sum);
+}
+
+__global__ void com2(const float* presums, float* sums, const float * in, int x){
+    __shared__ float cache[128];
+    float sum = 0;
+    int tid = threadIdx.x;
+    int a = x / blockDim.x;
+    int block_pos_x = blockIdx.x % a;
+    int block_pos_y = blockIdx.x / a;
+    cache[tid] = presums[block_pos_y * blockDim.x + tid];
+
+    __syncthreads();
+    for (int i = 0; i < 128; ++i){
+        int cur = x * block_pos_y * blockDim.x + block_pos_x * blockDim.x + tid + i * x;
+        sum += cache[i] * in[cur];
+    }
+
+    atomicAdd(&sums[tid + block_pos_x * blockDim.x], sum);
+}
+
+__global__ void  compute_presum( float * cpresums, int x){
+    const float gain = 6.0f;
+    const float z = sqrtf(3.f) - 2.f;
+    float z1 = z;
+    float z2 = powf(z, 2 * x - 2);
+    float iz = 1.f / z;
+
+    cpresums[0] = 0;
+    cpresums[x-1] = 0;
+    for (int j = 1; j < (x - 1); ++j) {
+        cpresums[j] = (z2 + z1);
+        z1 *= z;
+        z2 *= iz;
+    }
+}
+
+__device__ float* gpresums;
+__device__ float* sums;
+__device__ float* sums2;
 
 void solveGPU(float *in, float *out, int x, int y) {
-//    const float gain = 6.0f;
-//    const float z = sqrtf(3.f) - 2.f;
-//    float z1;
-    copy_data<<<(x*y)/256, 256>>>(in, out);
-    compute_lines<<<y/BLOCK_SIZE, BLOCK_SIZE>>>(in, out, x, y);
 
-    copy_data<<<(x*y)/256, 256>>>(out, out);
-    compute_cols<<<x/BLOCK_SIZE, BLOCK_SIZE>>>(in, out, x, y);
+//    copy_data<<<(x*y)/256, 256>>>(in, out);
 
-//
-//    for (int line = 0; line < y; line++) {
-//        float* myLine = out + (line * x);
-//
-//        // copy input data
-//        for (int i = 0; i < x; i++) {
-//            myLine[i] = in[i + (line * x)] * gain;
-//        }
-//
-//        // compute 'sum'
-//        float sum = (myLine[0] + powf(z, x)
-//                                 * myLine[x - 1]) * (1.f + z) / z;
-//        z1 = z;
-//        float z2 = powf(z, 2 * x - 2);
-//        float iz = 1.f / z;
-//        for (int j = 1; j < (x - 1); ++j) {
-//            sum += (z2 + z1) * myLine[j];
-//            z1 *= z;
-//            z2 *= iz;
-//        }
-//
-//        // iterate back and forth
-//        myLine[0] = sum * z / (1.f - powf(z, 2 * x));
-//        for (int j = 1; j < x; ++j) {
-//            myLine[j] += z * myLine[j - 1];
-//        }
-//        myLine[x - 1] *= z / (z - 1.f);
-//        for (int j = x - 2; 0 <= j; --j) {
-//            myLine[j] = z * (myLine[j + 1] - myLine[j]);
-//        }
-//    }
-//
-//    // process columns
-//    for (int col = 0; col < x; col++) {
-//        float *myCol = out + col;
-//
-//        // multiply by gain (input data are already copied)
-//        for (int i = 0; i < y; i++) {
-//            myCol[i * x] *= gain;
-//        }
-//
-//        // compute 'sum'
-//        float sum = (myCol[0 * x] + powf(z, y)
-//                                    * myCol[(y - 1) * x]) * (1.f + z) / z;
-//        z1 = z;
-//        float z2 = powf(z, 2 * y - 2);
-//        float iz = 1.f / z;
-//        for (int j = 1; j < (y - 1); ++j) {
-//            sum += (z2 + z1) * myCol[j * x];
-//            z1 *= z;
-//            z2 *= iz;
-//        }
-//
-//        // iterate back and forth
-//        myCol[0 * x] = sum * z / (1.f - powf(z, 2 * y));
-//        for (int j = 1; j < y; ++j) {
-//            myCol[j * x] += z * myCol[(j - 1) * x];
-//        }
-//        myCol[(y - 1) * x] *= z / (z - 1.f);
-//        for (int j = y - 2; 0 <= j; --j) {
-//            myCol[j * x] = z * (myCol[(j + 1) * x] - myCol[j * x]);
-//        }
-//    }
+    cudaMalloc(&gpresums, x * sizeof(float));
+    cudaMalloc(&sums, x * sizeof(float));
+    cudaMalloc(&sums2, x * sizeof(float));
+//    cudaMemset(sums, 0, x * sizeof(float));
+    compute_presum<<<1,1>>>(gpresums, x);
+    com<<<(x*y)/(128 * 128), 128>>>(gpresums, sums, in, x);
+
+    compute_lines<<<y/BLOCK_SIZE, BLOCK_SIZE>>>(in, out, x, y, sums);
+
+//    copy_data<<<(x*y)/256, 256>>>(in, out);
+    com2<<<(x*y)/(128 * 128), 128>>>(gpresums, sums2, out, x);
+    compute_cols<<<x/BLOCK_SIZE, BLOCK_SIZE>>>(out, out, x, y, sums2);
+
 }
 
